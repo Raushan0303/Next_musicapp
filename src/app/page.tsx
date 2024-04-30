6eb571695e7cfe6bcf989d544610869bd04d53fc
Raushan0303 <110516637+Raushan0303@users.noreply.github.com>
2024-04-30 09:43:23 +0530

    add foooter and course

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
